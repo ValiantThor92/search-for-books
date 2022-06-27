c071e7eb07a8ba6813f8ccbb758de2136a233b2c
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-06-27 13:18:27 -0700

    reconfigure server

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,7 +8,7 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +16,8 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req
+      // return res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
@@ -28,8 +29,9 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
+    return req
     // send to next endpoint
-    next();
+    //next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
